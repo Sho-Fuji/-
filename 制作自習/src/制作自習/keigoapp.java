579dbb74ac4ebb0f645474c2ffaf4f8911aeba93
nanba sho <kd1306946@st.kobedenshi.ac.jp>
2020-12-04 14:09:55 +0900

    制作用

---
+++ 制作自習/src/制作自習/keigoapp.java
@@ -0,0 +1,94 @@
+package 制作自習;
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+/**
+ * 
+ */
+
+/**
+ * @author 0H02020 難波聖
+ *
+ */
+public class keigoapp extends JFrame {
+
+	private JPanel contentPane;
+	private keigomenu menu=new keigomenu();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					keigoapp frame = new keigoapp();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public keigoapp() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.CYAN);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel DIspLabel = new JLabel("語の学び舎");
+		DIspLabel.setBounds(79, 45, 347, 57);
+		DIspLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		DIspLabel.setFont(new Font("HG行書体", Font.PLAIN, 39));
+		contentPane.add(DIspLabel);
+		
+		JLabel DispLabel1 = new JLabel("敬");
+		DispLabel1.setBounds(22, 13, 151, 77);
+		DispLabel1.setForeground(Color.RED);
+		DispLabel1.setHorizontalAlignment(SwingConstants.CENTER);
+		DispLabel1.setFont(new Font("HG行書体", Font.PLAIN, 99));
+		contentPane.add(DispLabel1);
+		
+		JButton StartButton = new JButton("START");
+		StartButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				menu.setVisible(true);
+			}
+		});
+		StartButton.setBounds(142, 150, 151, 41);
+		StartButton.setFont(new Font("Gill Sans Ultra Bold Condensed", Font.PLAIN, 25));
+		contentPane.add(StartButton);
+		
+		JButton EndButton = new JButton("終了");
+		EndButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		EndButton.setBounds(142, 201, 151, 41);
+		EndButton.setFont(new Font("Dialog", Font.BOLD, 25));
+		contentPane.add(EndButton);
+		
+		
+	
+	}
+
+}
---
+++ 制作自習/src/制作自習/keigomenu.java
@@ -0,0 +1,94 @@
+package 制作自習;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+/**
+ * 
+ */
+
+/**
+ * @author 0H02020 難波聖
+ *
+ */
+public class keigomenu extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			keigomenu dialog = new keigomenu();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public keigomenu() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel DispLabel = new JLabel("ゲームモード");
+		DispLabel.setFont(new Font("HG行書体", Font.PLAIN, 30));
+		DispLabel.setForeground(Color.RED);
+		DispLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		DispLabel.setBounds(109, 10, 215, 45);
+		contentPanel.add(DispLabel);
+		
+		JButton Button1 = new JButton("文法編");
+		Button1.setBounds(156, 65, 127, 36);
+		contentPanel.add(Button1);
+		
+		JButton Button2 = new JButton("実践編");
+		Button2.setBounds(156, 111, 127, 36);
+		contentPanel.add(Button2);
+		
+		JButton Button3 = new JButton("復習編");
+		Button3.setBounds(156, 157, 127, 36);
+		contentPanel.add(Button3);
+					
+		JButton CancelButton = new JButton("戻る");
+		CancelButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				setVisible(false);
+			}
+		});
+		CancelButton.setBounds(136, 210, 68, 35);
+		contentPanel.add(CancelButton);
+			
+		JButton EndButton = new JButton("終了");
+		EndButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		EndButton.setBounds(230, 210, 68, 35);
+		contentPanel.add(EndButton);
+		
+		
+		
+		
+	}
+}
