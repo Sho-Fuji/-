1f30aeddb6b1ea6c299bbd693a40b5ec1e260d33
FUJITA Shotaro <kd1249779@st.kobedenshi.ac.jp>
2020-12-23 16:14:35 +0900

    敬語（文法編）アップします。

---
+++ 制作自習/src/制作自習/gra12.java
@@ -0,0 +1,157 @@
+package 制作自習;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class gra12 extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton AnswerButton;
+	private JButton NextButton;
+	private JLabel kaisetuLabel;
+	private JPanel panel;
+	private JLabel lblNewLabel;
+	private JRadioButton ARadioButton;
+	private JRadioButton BRadioButton;
+	private JRadioButton CRadioButton;
+	private JRadioButton DRadioButton;
+	private ButtonGroup bg;
+	private JLabel MessageLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			gra12 dialog = new gra12();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public gra12() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		lblNewLabel = new JLabel("「会う」の尊敬語は？？");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("BIZ UDPゴシック", Font.BOLD, 20));
+		lblNewLabel.setBounds(34, -4, 319, 50);
+		contentPanel.add(lblNewLabel);
+		
+		panel = new JPanel();
+		panel.setBounds(33, 56, 393, 57);
+		contentPanel.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		ARadioButton = new JRadioButton("お目にかかる");
+		ARadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		ARadioButton.setSelected(true);
+		panel.add(ARadioButton);
+		
+		BRadioButton = new JRadioButton("会います");
+		BRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(BRadioButton);
+		
+		CRadioButton = new JRadioButton("伺う");
+		CRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(CRadioButton);
+		
+		DRadioButton = new JRadioButton("お会いになる");
+		DRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(DRadioButton);
+		
+		bg = new ButtonGroup();
+		bg.add(ARadioButton);
+		bg.add(BRadioButton);
+		bg.add(CRadioButton);
+		bg.add(DRadioButton);
+		
+		kaisetuLabel = new JLabel(" ");
+		kaisetuLabel.setVerticalAlignment(SwingConstants.TOP);
+		kaisetuLabel.setBounds(34, 165, 334, 95);
+		contentPanel.add(kaisetuLabel);
+		
+		JButton BackButton = new JButton("戻る");
+		BackButton.setActionCommand("Cancel");
+		BackButton.setBounds(363, 10, 63, 21);
+		contentPanel.add(BackButton);
+		
+		MessageLabel = new JLabel("");
+		MessageLabel.setFont(new Font("游ゴシック", Font.BOLD, 40));
+		MessageLabel.setBounds(34, 123, 334, 57);
+		contentPanel.add(MessageLabel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				AnswerButton = new JButton("解答");
+				AnswerButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(ARadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お会いになる」である。<br>「会います」は丁寧語であり、「お目にかかる」は謙譲語のため不適切。</html>"); 
+							MessageLabel.setText("× 不正解");
+						}
+						else if(BRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お会いになる」である。<br>「会います」は丁寧語であり、「お目にかかる」は謙譲語のため不適切。</html>"); 
+							MessageLabel.setText("× 不正解");
+						}
+						else if(CRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お会いになる」である。<br>「会います」は丁寧語であり、「お目にかかる」は謙譲語のため不適切。</html>"); 
+							MessageLabel.setText("× 不正解");
+						}
+						else if(DRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>正解です。<br>「会います」は丁寧語であり、「お目にかかる」は謙譲語のため不適切。</html>"); 
+							MessageLabel.setText("〇 正解");
+							MessageLabel.setForeground(Color.RED);
+						}			
+						ARadioButton.setEnabled(false);
+						BRadioButton.setEnabled(false);
+						CRadioButton.setEnabled(false);
+						DRadioButton.setEnabled(false);
+						AnswerButton.setEnabled(false);
+						NextButton.setEnabled(true);
+					}
+				});
+				AnswerButton.setActionCommand("OK");
+				buttonPane.add(AnswerButton);
+				getRootPane().setDefaultButton(AnswerButton);
+			}
+			{
+				NextButton = new JButton("次へ");
+				NextButton.setEnabled(false);
+				NextButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				NextButton.setActionCommand("Cancel");
+				buttonPane.add(NextButton);
+			}
+		}
+	}
+}
---
+++ 制作自習/src/制作自習/gra8.java
@@ -0,0 +1,158 @@
+package 制作自習;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class gra8 extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JButton AnswerButton;
+	private JButton NextButton;
+	private JLabel kaisetuLabel;
+	private JPanel panel;
+	private JLabel lblNewLabel;
+	private JRadioButton ARadioButton;
+	private JRadioButton BRadioButton;
+	private JRadioButton CRadioButton;
+	private JRadioButton DRadioButton;
+	private ButtonGroup bg;
+	private JLabel MessageLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			gra8 dialog = new gra8();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public gra8() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		lblNewLabel = new JLabel("「もらう」の謙譲語は？？");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("BIZ UDPゴシック", Font.BOLD, 20));
+		lblNewLabel.setBounds(34, 10, 319, 50);
+		contentPanel.add(lblNewLabel);
+		
+		panel = new JPanel();
+		panel.setBounds(10, 70, 416, 57);
+		contentPanel.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		ARadioButton = new JRadioButton("いただく");
+		ARadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		ARadioButton.setSelected(true);
+		panel.add(ARadioButton);
+		
+		BRadioButton = new JRadioButton("もらいます");
+		BRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(BRadioButton);
+		
+		CRadioButton = new JRadioButton("お受けになる");
+		CRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(CRadioButton);
+		
+		DRadioButton = new JRadioButton("ちょうだいする");
+		DRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(DRadioButton);
+		
+		bg = new ButtonGroup();
+		bg.add(ARadioButton);
+		bg.add(BRadioButton);
+		bg.add(CRadioButton);
+		bg.add(DRadioButton);
+		
+		kaisetuLabel = new JLabel(" ");
+		kaisetuLabel.setVerticalAlignment(SwingConstants.TOP);
+		kaisetuLabel.setBounds(10, 177, 392, 83);
+		contentPanel.add(kaisetuLabel);
+		
+		JButton BackButton = new JButton("戻る");
+		BackButton.setActionCommand("Cancel");
+		BackButton.setBounds(363, 10, 63, 21);
+		contentPanel.add(BackButton);
+		
+		MessageLabel = new JLabel("");
+		MessageLabel.setFont(new Font("游ゴシック", Font.BOLD, 40));
+		MessageLabel.setBounds(34, 137, 334, 57);
+		contentPanel.add(MessageLabel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				AnswerButton = new JButton("解答");
+				AnswerButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(ARadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お受けになる」である。<br>「いただく」と「ちょうだいする」は謙譲語であり、「食べます」はて丁寧語である。</html>"); 
+							MessageLabel.setText("× 不正解");
+						}
+						else if(BRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お受けになる」である。<br>「いただく」と「ちょうだいする」は謙譲語であり、「食べます」はて丁寧語である。</html>"); 
+							MessageLabel.setText("× 不正解");
+						}
+						else if(CRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>正解です。<br>「いただく」と「ちょうだいする」は謙譲語であり、「食べます」はて丁寧語である。</html>"); 
+							MessageLabel.setText("〇 正解");
+							MessageLabel.setForeground(Color.RED);
+						}
+						else if(DRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は、”お客様”を表す「お受けになる」である。<br>「いただく」と「ちょうだいする」は謙譲語であり、「食べます」はて丁寧語である。</html>");
+							MessageLabel.setText("× 不正解");
+						}			
+						ARadioButton.setEnabled(false);
+						BRadioButton.setEnabled(false);
+						CRadioButton.setEnabled(false);
+						DRadioButton.setEnabled(false);
+						AnswerButton.setEnabled(false);
+						NextButton.setEnabled(true);
+					}
+				});
+				AnswerButton.setActionCommand("OK");
+				buttonPane.add(AnswerButton);
+				getRootPane().setDefaultButton(AnswerButton);
+			}
+			{
+				NextButton = new JButton("次へ");
+				NextButton.setEnabled(false);
+				NextButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				NextButton.setActionCommand("Cancel");
+				buttonPane.add(NextButton);
+			}
+		}
+	}
+}
