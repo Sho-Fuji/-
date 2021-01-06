8592a951fedf0db600ba529d1669076a65c4d1e2
Angga <Angga@DESKTOP-E7J2U3J>
2021-01-06 21:13:13 +0900

    実践問題アップしました

---
+++ 制作自習/src/制作自習/jissen9.java
@@ -0,0 +1,138 @@
+package 制作自習;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JRadioButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+
+public class jissen9 extends JDialog {
+	private final JPanel contentPanel = new JPanel();
+	private JButton AnswerButton;
+	private JButton NextButton;
+	private JLabel kaisetuLabel;
+	private JPanel panel;
+	private JLabel lblNewLabel;
+	private JRadioButton ARadioButton;
+	private JRadioButton BRadioButton;
+	private JRadioButton CRadioButton;
+	private JRadioButton DRadioButton;
+	private ButtonGroup bg;
+	private JLabel MessageLabel;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			jissen9 dialog = new jissen9();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public jissen9() {
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		lblNewLabel = new JLabel("私はリュウと...");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel.setForeground(Color.RED);
+		lblNewLabel.setFont(new Font("BIZ UDPゴシック", Font.BOLD, 20));
+		lblNewLabel.setBounds(34, -1, 319, 50);
+		contentPanel.add(lblNewLabel);
+		
+		panel = new JPanel();
+		panel.setBounds(63, 59, 346, 57);
+		contentPanel.add(panel);
+		panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		
+		ARadioButton = new JRadioButton("<html>いいます。<html>");
+		ARadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		ARadioButton.setSelected(true);
+		panel.add(ARadioButton);
+		
+		BRadioButton = new JRadioButton("<html>申します。<html>");
+		BRadioButton.setFont(new Font("UD デジタル 教科書体 N-B", Font.PLAIN, 14));
+		panel.add(BRadioButton);
+		
+		bg = new ButtonGroup();
+		bg.add(ARadioButton);
+		bg.add(BRadioButton);
+		
+		kaisetuLabel = new JLabel(" ");
+		kaisetuLabel.setVerticalAlignment(SwingConstants.TOP);
+		kaisetuLabel.setBounds(44, 168, 324, 140);
+		contentPanel.add(kaisetuLabel);
+		
+		JButton BackButton = new JButton("戻る");
+		BackButton.setActionCommand("Cancel");
+		BackButton.setBounds(363, 10, 63, 21);
+		contentPanel.add(BackButton);
+		
+		MessageLabel = new JLabel("");
+		MessageLabel.setFont(new Font("游ゴシック", Font.BOLD, 40));
+		MessageLabel.setBounds(34, 128, 334, 57);
+		contentPanel.add(MessageLabel);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				AnswerButton = new JButton("解答");
+				AnswerButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						if(ARadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>不正解です。正解は「申します。」である</html>");
+							MessageLabel.setText("× 不正解");
+							MessageLabel.setForeground(Color.RED);
+						}
+						else if(BRadioButton.isSelected()) {
+							kaisetuLabel.setText("<html>正解です。</html>");
+							MessageLabel.setText("〇 正解");
+						
+						}			
+						ARadioButton.setEnabled(false);
+						BRadioButton.setEnabled(false);
+						AnswerButton.setEnabled(false);
+						NextButton.setEnabled(true);
+					}
+				});
+				AnswerButton.setActionCommand("OK");
+				buttonPane.add(AnswerButton);
+				getRootPane().setDefaultButton(AnswerButton);
+			}
+			{
+				NextButton = new JButton("次へ");
+				NextButton.setEnabled(false);
+				NextButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+					}
+				});
+				NextButton.setActionCommand("Cancel");
+				buttonPane.add(NextButton);
+			}
+		}
+	}
+}
