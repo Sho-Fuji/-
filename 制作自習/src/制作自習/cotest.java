1f3598bdc6f6053c426ed56d6dbc2f9577f7513d
nanba sho <kd1306946@st.kobedenshi.ac.jp>
2021-01-12 00:35:01 +0900

    正解率のダイアログ

--- 制作自習/src/制作自習/cotest.java
+++ 制作自習/src/制作自習/cotest.java
@@ -4,6 +4,7 @@
 package 制作自習;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.FlowLayout;
 
 import javax.swing.JButton;
@@ -13,6 +14,7 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 /**
  * @author 0H02020 難波聖
@@ -24,6 +26,7 @@ public class cotest extends JDialog {
 	private JLabel aaa;
 	public keigomenu menu=new keigomenu();
 	public int count=0;
+	private JButton cancelButton;
 	/**
 	 * Launch the application.
 	 */
@@ -47,21 +50,35 @@ public class cotest extends JDialog {
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		{
-			aaa= new JLabel("New label");
-			aaa.setBounds(84, 10, 264, 98);
+			aaa= new JLabel("");
+			aaa.setFont(new Font("MS UI Gothic", Font.PLAIN, 43));
+			aaa.setBounds(126, 124, 210, 98);
 			contentPanel.add(aaa);
+		}
+		
+		JLabel lblNewLabel = new JLabel("あなたの敬語理解度は");
+		lblNewLabel.setFont(new Font("MS UI Gothic", Font.PLAIN, 24));
+		lblNewLabel.setBounds(58, 26, 255, 46);
+		contentPanel.add(lblNewLabel);
+		{
+			JLabel lblNewLabel_1 = new JLabel("10点中");
+			lblNewLabel_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 24));
+			lblNewLabel_1.setBounds(68, 82, 137, 46);
+			contentPanel.add(lblNewLabel_1);
 		}
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
 			{
-				JButton okButton = new JButton("OK");
+				JButton okButton = new JButton("表示");
 				okButton.addActionListener(new ActionListener() {
 					public void actionPerformed(ActionEvent e) {
 						String A=String.valueOf(count);
-						aaa.setText(A);
-						
+						aaa.setText(A+"点です");
+						aaa.setForeground(Color.RED);
+						cancelButton.setEnabled(true);
+						okButton.setEnabled(false);
 					}
 				});
 				okButton.setActionCommand("OK");
@@ -69,11 +86,17 @@ public class cotest extends JDialog {
 				getRootPane().setDefaultButton(okButton);
 			}
 			{
-				JButton cancelButton = new JButton("Cancel");
+				cancelButton = new JButton("メニューへ");
+				cancelButton.setEnabled(false);
+				cancelButton.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						setVisible(false);
+						menu.setVisible(true);
+					}
+				});
 				cancelButton.setActionCommand("Cancel");
 				buttonPane.add(cancelButton);
 			}
 		}
 	}
-
 }
